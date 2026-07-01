625c7c090623ebf86fccb10dc7e47cef855bb968
Brian Egan <beforeguard@gmail.com>
2026-07-01 07:16:31 -0700

    Implement logging functions for combat messages and summaries

--- CombatTracker/Combat.lua
+++ CombatTracker/Combat.lua
@@ -1,28 +1,3 @@
-function CombatTracker:LogMessage(text)
-    print("|cff00ff00[Combat Tracker]|r " .. text)
-end
-
-function CombatTracker.LogSummary()
-    if not CombatTracker.currentFight then
-        CombatTracker:LogMessage("No combat data available.")
-        return
-    end
-
-    local fight = CombatTracker.currentFight
-    CombatTracker:LogMessage(
-        string.format(
-            "Duration: %.2f seconds, Damage: %d, Healing: %d, Deaths: %d, Kills: %d",
-            fight.duration,
-            fight.damage,
-            fight.healing,
-            fight.deaths,
-            fight.kills
-        )
-    )
-    CombatTracker:LogMessage(
-        string.format("Total fights this session: %d", #CombatTracker.session.fights))
-end
-
 function CombatTracker:StartCombat()
     self.inCombat = true
 
